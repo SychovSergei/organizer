4558f22f93efe1c120daf5a14b47f85e77f19096
Sychov Sergei <sychov.sergei@gmail.com>
2022-04-30 15:13:38 +0300

    Created three components Calendar, Selector, Organizer(tasks list)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+import { MomentPipe } from "./shared/moment.pipe";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
